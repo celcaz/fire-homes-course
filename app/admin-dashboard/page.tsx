f2afd8ed7de9d889550d5ab85e529913d2804ece
Celio <celiocazares22@gmail.com>
2025-10-27 21:07:44 -0300

    27. Render the properties list in a table

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,14 +1,10 @@
 import { Breadcrumbs } from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
-import { getProperties } from "@/data/properties";
 import { PlusCircleIcon } from "lucide-react";
 import Link from "next/link";
+import PropertiesTable from "./properties-table";
 
 export default async function AdminDashboard() {
-  const data = await getProperties();
-
-  console.log({ data });
-
   return (
     <div>
       <Breadcrumbs items={[{ label: "Dashboard" }]} />
@@ -18,6 +14,7 @@ export default async function AdminDashboard() {
           <PlusCircleIcon /> New Property
         </Link>
       </Button>
+      <PropertiesTable />
     </div>
   );
 }
