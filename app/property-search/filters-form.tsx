4b594eee85f3ac460531ed1ffe1eed485cb6a12f
Celio <celiocazares22@gmail.com>
2026-01-13 17:01:34 -0300

    46. Hook up the search filters to the URL and create firestore indexes

--- app/property-search/filters-form.tsx
+++ app/property-search/filters-form.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -14,17 +15,32 @@ const formSchema = z.object({
 });
 
 export default function FiltersForm() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      maxPrice: "",
-      minPrice: "",
-      minBedrooms: "",
+      maxPrice: searchParams.get("maxPrice") ?? "",
+      minPrice: searchParams.get("minPrice") ?? "",
+      minBedrooms: searchParams.get("minBedrooms") ?? "",
     },
   });
 
   const handleSubmit = async (data: z.infer<typeof formSchema>) => {
     console.log({ data });
+    const newSearchParams = new URLSearchParams();
+
+    if (data.minBedrooms) {
+      newSearchParams.set("minBedrooms", data.minBedrooms);
+    }
+    if (data.minPrice) {
+      newSearchParams.set("minPrice", data.minPrice);
+    }
+    if (data.maxPrice) {
+      newSearchParams.set("maxPrice", data.maxPrice);
+    }
+    newSearchParams.set("page", "1");
+    router.push(`/property-search?${newSearchParams.toString()}`);
   };
 
   return (
